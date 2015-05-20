0f856856328a9bfde38242faf3317cd0fbec2fc9
victor <victor-morel@hotmail.fr>
2015-05-20 12:06:37 +0200

    ajout méthodes robot, et un peu dans manager

--- src/model/robot/Manager.java
+++ src/model/robot/Manager.java
@@ -3,6 +3,7 @@ package model.robot;
 import model.graph.Graph;
 import model.graph.Node;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -13,9 +14,14 @@ public class Manager {
     private List<Robot> bots;
     private Graph g;
 
-    public void getPositions (){ //doit retourner pour chaque robot sa distance aux noeuds en feu, ou pour chaque feu sa distance des robots
+    public void getPositions (){
+        //doit retourner pour chaque robot sa distance aux noeuds en feu, ou pour chaque feu sa distance des robots
         //ça peut être pas mal de retourner pour chaque robot sa distance aux noeuds en feu (car plus simple), mais pour choisir
         //c'est peut être plus judicieux de savoir pour chaque feu quel robot est le plus proche
+        HashMap<Integer,HashMap<Integer,Double>> listDistance = new HashMap<>(null);
+        for (Robot bot : bots){
+          //  HashMap<bot.getId(), bot.getNearFires()>;
+        }
 
     }
 
--- src/model/robot/Robot.java
+++ src/model/robot/Robot.java
@@ -9,25 +9,42 @@ import java.util.HashMap;
  * Created by victor on 20/05/15.
  */
 public abstract class Robot {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public enum TypeRecherche{
         dijkstra, astar
     }
 
     private TypeRecherche type;
+    private int id;
     private Node node;
     private boolean busy;
-    private int capacity;
+    private double capacity;
     private Graph g;
 
-    public Robot (TypeRecherche _type, int _capacity){
+    public Robot (TypeRecherche _type, double _capacity){
         this.type = _type;
         this.busy = false;
         this.capacity = _capacity;
     }
 
-    public void getNearFires (){ //doit retourner une liste avec pour chaque noeud en feu, une distance
-
+    public HashMap<Integer,Double> getNearFires (){ //doit retourner une liste avec pour chaque noeud en feu, une distance
+        HashMap<Integer,Double> fireList = new HashMap<>(null);
+        for (Node fn : g.getAllFireNodes()){
+            //appel de strategy entre le noeud du robot, le noeud en feu, avec comme paramètre le type de recherche
+        }
+        return fireList;
     }
 
+    public void extinguish (Node inFlames){
+        inFlames.cooling(capacity);
+    }
 
 }
