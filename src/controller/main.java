d705042700263fab1abb3dd0ae59df2c8d742679
theodury <tdury2@gmail.com>
2015-05-31 22:44:54 +0200

    add Action button

--- src/controller/main.java
+++ src/controller/main.java
@@ -4,8 +4,6 @@ public class main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World");
-
         Controller controller = new Controller(config.path);
 
     }
