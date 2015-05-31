2c46926c494dc97a54eb4bc876b4746d17a80652
theodury <tdury2@gmail.com>
2015-05-31 19:24:10 +0200

    creation du fichier de configuration pour le changement des parametres

---
+++ src/controller/config.java
@@ -0,0 +1,12 @@
+package controller;
+
+import java.lang.String;public class config {
+
+    //theo controller.config
+    public static String path;
+
+    static {
+        path = "C:\\Users\\TD\\Desktop\\Asimov-master\\data\\mapsixieme.xml";
+    }
+
+}
