6863f67d904f7543d44652688ed57370f5464c79
theodury <tdury2@gmail.com>
2015-05-31 22:03:59 +0200

    refactoring Code

--- src/controller/main.java
+++ src/controller/main.java
@@ -1,17 +1,13 @@
 package controller;
 
-import controller.config;
-import view.GUI;
-import java.lang.String;
-import java.lang.System;
-
 public class main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello, World");
 
-        GUI myGUI = new GUI(config.path);
+        Controller controller = new Controller(config.path);
+
     }
 
 }
