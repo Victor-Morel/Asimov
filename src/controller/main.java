54749a636c4649f8a6189930d4afa42bdf3de02a
theodury <tdury2@gmail.com>
2015-05-31 19:25:06 +0200

    ficher d'execution pour tester la GUI

---
+++ src/controller/main.java
@@ -0,0 +1,14 @@
+package controller;
+
+import controller.config;import view.GUI;import java.lang.String;import java.lang.System;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World");
+
+        GUI myGUI = new GUI(config.path);
+    }
+
+}
