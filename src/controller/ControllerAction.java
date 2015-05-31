e66bd58d6abe805dd54e646187c7668ab63bc569
theodury <tdury2@gmail.com>
2015-05-31 22:43:55 +0200

    add Action button

--- src/controller/ControllerAction.java
+++ src/controller/ControllerAction.java
@@ -10,28 +10,40 @@ import java.awt.event.ActionListener;
 public class ControllerAction implements ActionListener {
 
     String path;
+    Boolean node, fire, plat, inonder, escarpe;
 
     public ControllerAction(String _path) {
         super();
         this.path = _path;
+
+        initialization();
+    }
+
+    private void initialization() {
+        node = false;
+        fire = false;
+        plat = false;
+        inonder = false;
+        escarpe = false;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().matches("Noeud")) {
+            node = true;
 
         }
         if (e.getActionCommand().matches("Feu")) {
-
+            fire = true;
         }
         if (e.getActionCommand().matches("Plat")) {
-
+            plat = true;
         }
         if (e.getActionCommand().matches("Escarpe")) {
-
+            inonder = true;
         }
         if (e.getActionCommand().matches("Inonde")) {
-
+            escarpe = true;
         }
         if (e.getActionCommand().matches("Save")) {
 
@@ -41,6 +53,31 @@ public class ControllerAction implements ActionListener {
             new FileManager(chooseFile.selectFile(this.path));
         }
 
+    }
+
+    public void addElement(int x, int y) {
+        if (node) {
+
+            //TODO create new node
+            System.out.println("Create new Node");
+            node = false;
+        }
+        if (fire) {
+
+            fire = false;
+        }
+        if (plat) {
+
+            plat = false;
+        }
+        if (escarpe) {
+
+            escarpe = false;
+        }
+        if (inonder) {
+
+            inonder = false;
+        }
     }
 }
 
