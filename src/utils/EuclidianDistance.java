480a8bdd86e7de0617ad2cc06705d7c6b45f1551
loganpaul <logan.paul73@gmail.com>
2015-06-02 14:44:56 +0200

    add class EuclidianDistance

--- src/utils/EuclidianDistance.java
+++ src/utils/EuclidianDistance.java
@@ -1,13 +1,12 @@
 package utils;
 
-import java.awt.*;
-
+import model.graph.Node;
 /**
  * Created by Logan Paul on 02/06/2015.
  */
 public class EuclidianDistance {
 
-    public static double getDistance(Point a, Point b) {
+    public static double getDistance(Node a, Node b) {
         return Math.abs(a.getX() - b.getX()) + Math.abs(a.getY() - b.getY());
     }
 }
