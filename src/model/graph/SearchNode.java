2488293f5c55475f312d4d179315f5aadb4313a9
loganpaul <logan.paul73@gmail.com>
2015-06-05 00:41:35 +0200

    A* coded ==> need to be tested (dijkstra too)

--- src/model/graph/SearchNode.java
+++ src/model/graph/SearchNode.java
@@ -5,12 +5,36 @@ package model.graph;
  */
 public class SearchNode {
     private Node node;
-    private Node parent;
-    private int value;
+    private SearchNode parent;
+    private double value;
 
-    public SearchNode(Node node, Node parent, int value) {
+    public SearchNode(Node node, SearchNode parent, double value) {
+        this.setNode(node);
+        this.setParent(parent);
+        this.setValue(value);
+    }
+
+    public Node getNode() {
+        return node;
+    }
+
+    public void setNode(Node node) {
         this.node = node;
+    }
+
+    public SearchNode getParent() {
+        return parent;
+    }
+
+    public void setParent(SearchNode parent) {
         this.parent = parent;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
         this.value = value;
     }
 }
--- src/model/research/AStar.java
+++ src/model/research/AStar.java
@@ -3,6 +3,7 @@ package model.research;
 import model.graph.Graph;
 import model.graph.Node;
 import model.graph.SearchNode;
+import utils.EuclidianDistance;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,12 +12,71 @@ import java.util.List;
  * Created by victor on 20/05/15.
  */
 public class AStar extends Strategy {
-    private List<SearchNode> openList;
+    private List<SearchNode> openedList;
     private List<SearchNode> closedList;
 
     public AStar(Graph g, Node n_start, Node n_dest) {
-        openList = new ArrayList<SearchNode>();
+        openedList = new ArrayList<SearchNode>();
         closedList = new ArrayList<SearchNode>();
+
+        SearchNode searchNodeStart = new SearchNode(n_start,null,0);
+        SearchNode bestSn = null;
+
+        do {
+            for(Node n : g.getNextNodes(searchNodeStart.getNode())) {
+                SearchNode sn = new SearchNode(n,searchNodeStart,EuclidianDistance.getDistance(n,n_dest));
+                int id = -1;
+                for(int i = 0; i < closedList.size(); i++) {
+                    if(closedList.get(i).getNode().equals(sn.getNode())) {
+                        id = i;
+                        break;
+                    }
+                }
+                if(id >= 0) {
+                    break;
+                }
+                for(int i = 0; i < closedList.size(); i++) {
+                    if(closedList.get(i).getNode().equals(sn.getNode())) {
+                        id = i;
+                        break;
+                    }
+                }
+                if(id >= 0) {
+                    if(sn.getValue() < openedList.get(id).getValue()) {
+                        openedList.remove(id);
+                        openedList.add(id,sn);
+                    }
+                }
+                else {
+                    openedList.add(sn);
+                }
+            }
+
+            if(openedList.isEmpty()) {
+                this.resultGraph = new Graph();
+                break;
+            }
+            bestSn = openedList.get(0);
+            for(SearchNode sn : openedList) {
+                if (sn.getValue() < bestSn.getValue()) {
+                    bestSn = sn;
+                }
+            }
+            openedList.remove(bestSn);
+            closedList.add(bestSn);
+
+        }while(bestSn.getNode().equals(n_dest));
+
+        if(bestSn.getNode().equals(n_dest)) {
+            this.resultGraph = new Graph();
+            resultGraph.addNode(bestSn.getNode());
+            SearchNode currentNode = bestSn;
+            while(null != currentNode.getParent()) {
+                resultGraph.addEdge(g.getEdge(currentNode.getNode(),currentNode.getParent().getNode()));
+                resultGraph.addNode(currentNode.getNode());
+            }
+            resultGraph.addNode(currentNode.getNode());
+        }
     }
 
 }
