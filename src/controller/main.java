86d50e281c4481f3342ec7878bc2faf90e09bb68
victor <victor-morel@hotmail.fr>
2015-06-05 17:59:51 +0200

    correction bug save xml

--- src/controller/main.java
+++ src/controller/main.java
@@ -4,9 +4,6 @@ public class main {
     public static void main(String[] args) {
 
         Controller controller = new Controller(config.path);
-
-        //    Node n = new Node(new StringLabel("A"), 1, 2);
-
     }
 
 }
